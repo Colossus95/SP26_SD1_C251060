4c220e86e4b4b3ee0eb281e8df9ed40f7a4941f8
Yousuf Zulqernine <231623641+Colossus95@users.noreply.github.com>
2026-03-02 01:34:24 +0600

    palindrome checker

---
+++ Cwork8.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Cwork8 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String str = input.next();
+        int size = str.length();
+
+        boolean f = true;
+
+        for (int i = 0; i < size / 2; i++) {
+            if (str.charAt(i) != str.charAt(size - i - 1)) {
+                f = false;
+                break;
+            }
+        }
+
+        if (f) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        input.close();
+    }
+}
